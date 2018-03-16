8b4238f4351f4fd1daae82033a289fb74b29063f
lilithakobyan <lilithakobyan1994@mail.ru>
2018-03-16 13:37:38 +0400

    add app body

---
+++ Forum/Forum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Forum.Startup))]
+namespace Forum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
