fee0824344751b83baa22e0f134793c41e45f1d1
lilithakobyan <lilithakobyan1994@mail.ru>
2018-03-20 23:56:55 +0400

    add database

---
+++ DatabaseProject/dbo/Tables/Posts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Posts] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Text]     NVARCHAR (50)  NOT NULL,
+    [ThreadId] INT            NOT NULL,
+    [UserId]   NVARCHAR (128) NOT NULL,
+    [UserName] NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([ThreadId]) REFERENCES [dbo].[Threads] ([Id]),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
---
+++ DatabaseProject/dbo/Tables/Threads.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Threads] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Name]            NVARCHAR (50)  NOT NULL,
+    [TextDescription] NVARCHAR (MAX) NOT NULL,
+    [TopicId]         INT            NULL,
+    [CreatedAt]       DATE           NULL,
+    [UserId]          NVARCHAR (128) NOT NULL,
+    [UserName]        NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([TopicId]) REFERENCES [dbo].[Topics] ([Id]),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
